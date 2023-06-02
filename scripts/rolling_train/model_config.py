ea4ad748738a8619b975b231771c583c3fe4b891
Erik Harutyunyan <erohar1311@gmail.com>
2023-06-02 15:26:22 +0400

    gats orig config

--- scripts/rolling_train/model_config.py
+++ scripts/rolling_train/model_config.py
@@ -12,10 +12,10 @@ model_config = {
                 "weight_decay": 0.001,
                 "early_stop": 10,
                 "metric": "loss",
-                "loss": "precise_margin_ranking_w_mse",
-                "lamb1_precise_margin_ranking": 0.8,
-                "lamb2_precise_margin_ranking": 0.2,
-                "func_precise_margin_ranking": "linear",
+                "loss": "mse",
+                #"lamb1_precise_margin_ranking": 0.8,
+                #"lamb2_precise_margin_ranking": 0.2,
+                #"func_precise_margin_ranking": "linear",
                 "base_model": "LSTM",
                 "model_path": "/home/erohar/qlib/examples/benchmarks/LSTM/csi300_lstm_ts.pkl",
                 "GPU": 0,
@@ -79,6 +79,17 @@ model_config = {
                                 "class": "Fillna",
                                 "kwargs": {
                                     "fields_group": "feature"
+                                }
+                            }
+                        ],
+                        "learn_processors": [
+                            {
+                                "class": "DropnaLabel"
+                            },
+                            {
+                                "class": "CSRankNorm",
+                                "kwargs": {
+                                    "fields_group": "label"
                                 }
                             }
                         ],
